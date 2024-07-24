aeb7f4f69c1b078643abc20339bcd22ef83144ca
Nguyen Anh Tien <34856010+tienna@users.noreply.github.com>
2024-07-24 21:49:37 +0700

    Update mithril.sh

--- mithril.sh
+++ mithril.sh
@@ -4,4 +4,4 @@ export AGGREGATOR_ENDPOINT=https://aggregator.pre-release-preview.api.mithril.ne
 export GENESIS_VERIFICATION_KEY=$(wget -q -O - https://raw.githubusercontent.com/input-output-hk/mithril/main/mithril-infra/configuration/pre-release-preview/genesis.vkey)
 export SNAPSHOT_DIGEST=latest
 mithril-client cardano-db download $SNAPSHOT_DIGEST
-nohup bash -c 'cardano-node run --config /opt/cardano/config/preview/config.json --database-path "$(pwd)/data/testnet/$SNAPSHOT_DIGEST/db" --socket-path /opt/cardano/config/preview/db/node.socket --host-addr 0.0.0.0 --port 3001 --topology /opt/cardano/config/preview/topology.json '>/dev/null 2>&1
+cardano-node run --config /opt/cardano/config/preview/config.json --database-path "$(pwd)/db" --socket-path /opt/cardano/config/preview/db/node.socket --host-addr 127.0.0.1 --port 3008 --topology /opt/cardano/config/preview/topology.json
