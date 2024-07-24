53216b2d11d8cd2a70d91d3bc6d38cff9ed970cc
Nguyen Anh Tien <34856010+tienna@users.noreply.github.com>
2024-07-24 21:02:09 +0700

    Create mithril.sh

---
+++ .devcontainer/mithril.sh
@@ -0,0 +1,5 @@
+export CARDANO_NETWORK=preview
+export AGGREGATOR_ENDPOINT=https://aggregator.pre-release-preview.api.mithril.network/aggregator
+export GENESIS_VERIFICATION_KEY=$(wget -q -O - https://raw.githubusercontent.com/input-output-hk/mithril/main/mithril-infra/configuration/pre-release-preview/genesis.vkey)
+export SNAPSHOT_DIGEST=latest
+mithril-client cardano-db download $SNAPSHOT_DIGEST
